2018c65183cb8790a9e1b9cbddd7414d06a7d320
indigoparadox <indigo.repos@interfinitydynamics.info>
2021-01-23 02:37:23 -0500

    Initial commit.

---
+++ sensors.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import board
+import time
+import logging
+import threading
+import json
+import argparse
+from adafruit_sgp40 import SGP40
+from adafruit_pm25.i2c import PM25_I2C
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from configparser import ConfigParser
+
+class SensorThread( threading.Thread ):
+
+    def __init__( self, i2c ):
+        super().__init__()
+        self.running = True
+        self.daemon = True
+        self.pm25 = PM25_I2C( i2c )
+        self.sgp40 = SGP40( i2c )
+        self.lock = threading.Lock()
+
+    def run( self ):
+
+        logger = logging.getLogger( 'sensors.run' )
+
+        while self.running:
+
+            with self.lock:
+                self.airq = self.pm25.read()
+                new_airq = {}
+                for key in self.airq:
+                    new_key = key.replace( ' ', '_' )
+                    val = self.airq[key]
+                    new_airq[new_key] = val
+                self.airq = new_airq
+                self.airq['tvoc'] = self.sgp40.raw
+                self.airq['timestamp'] = int( time.time() )
+
+                logger.debug( 'updating...' )
+
+            time.sleep( 5 )
+
+class SensorHTTPHandler( SimpleHTTPRequestHandler ):
+
+    def do_GET( self ):
+
+        logger = logging.getLogger( 'http.get' )
+
+        self.send_response( 200 )
+        self.send_header( 'Content-type', 'text/json' )
+        self.end_headers()
+
+        airq = self.server.sensor_thread.airq
+        logger.debug( 'response readings updated {}...'.format(
+            airq['timestamp'] ) )
+        airq_json = json.dumps( airq )
+        self.wfile.write( airq_json.encode( 'utf-8' ) )
+
+    def log_message( self, format, *args ):
+        return
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument( '-c', '--config', action='store' )
+
+    parser.add_argument( '-v', '--verbose', action='store_true' )
+
+    args = parser.parse_args()
+
+    level = logging.ERROR
+    if args.verbose:
+        level=logging.DEBUG
+    logging.basicConfig( level=level )
+    l = logging.getLogger( 'airq' )
+
+    config = ConfigParser()
+    config.read( args.config )
+
+    i2c = board.I2C()
+
+    listen = (config['server']['listen'], config.getint( 'server', 'port' ))
+    server = ThreadingHTTPServer( listen, SensorHTTPHandler )
+    server.sensor_thread = SensorThread( i2c )
+    server.sensor_thread.start()
+    server.serve_forever()
+
+if '__main__' == __name__:
+    main()
+
